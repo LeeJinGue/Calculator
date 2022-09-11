77093148a382385643466eef34ab8a2e6f59c650
LeeJinGue <hippo9851@gmail.com>
2022-09-11 21:55:23 +0900

    feat: DEL, 결과(=)기능 추가
    - 결과 기능에 덧셈, 뺄셈, 곱셈, 나눗셈, 나눗셈(나머지), 제곱, 소숫점 연산 구현

--- src/components/CalculatorPage/CalculatorPage.tsx
+++ src/components/CalculatorPage/CalculatorPage.tsx
@@ -2,14 +2,67 @@ import { Box, Center, Grid, GridItem } from "@chakra-ui/react";
 import CalButton from "../common/CalButton";
 import { CALARRAY } from "../../constants/array";
 import { MouseEventHandler,MouseEvent, useState } from "react";
+const getAnswer = (expression:string) => {
+  const nums = expression.split(/\D/)
+  const operators = expression.split(/[\d]+/)
+  // 1. 숫자를 모아둔 nums배열
+  // 2. 연산자를 모아둔 operators배열
+  operators.pop()
+  operators.shift()
+  // 연산자 split하면 앞,뒤에 공백 붙어서 하나씩 제거
+  let result:number = 0
+  nums.forEach((val, index) => {
+    if(index === 0) result = Number.parseInt(nums[0])
+    else{
+      switch(operators[index-1]){
+        case "+":
+          // console.log(`덧셈: ${result} + ${nums[index]}`)
+          result += Number.parseInt(nums[index])
+          break;
+        case "-":
+          // console.log(`뺄셈: ${result} - ${nums[index]}`)
+          result -= Number.parseInt(nums[index])
+          break;
+        case "*":
+          // console.log(`곱셈: ${result} * ${nums[index]}`)
+          result *= Number.parseInt(nums[index])
+          break;
+        case "/":
+          // console.log(`나눗셈: ${result} / ${nums[index]}`)
+          result /= Number.parseInt(nums[index])
+          break;
+        case "%":
+          // console.log(`나눗셈 나머지: ${result} % ${nums[index]}`)
+          result %= Number.parseInt(nums[index])
+          break;
+        case "^":
+          // console.log(`제곱: ${result} ^ ${nums[index]}`)
+          result **= Number.parseInt(nums[index])
+          break;
+        case ".":
+          const right=Number.parseInt(nums[index])/10**(nums[index].length)
+          console.log(`소숫점: ${result}+${right}`)
+          result += right;
+          break;
+      }
+    }
+  })
+  return result.toString();
+}
 const CalculatorPage:React.FC = () => {
   const [answer, setAnswer] = useState("")
   const handleCalButtonClick = (val:string) => {
     switch(val){
-      case "AC":
+      case "AC":  // AC : All Clear 다 지우기
         setAnswer("")
         break
-      default:
+      case "DEL":  // DEL : Delete 하나 지우기
+        setAnswer(prev => prev.substring(0, prev.length-1))
+        break
+      case "=":  // = : 결과
+        setAnswer(prev => getAnswer(prev))
+        break
+      default:  // default: 숫자들 0~9
         setAnswer(prev => prev + val)
         break
     }
